889fa35484f6357156475a3784cd28c9333b57a7
00009028 <72851911+00009028@users.noreply.github.com>
2021-03-25 20:58:53 +0500

    add view folder with layout and home pugs file

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
- 
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'pug')
+
+// localhost:3000
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(3000, err => {
+    if (err) console.log(err)
+
+    console.log('Server is running on port 3000 ...')
+})
